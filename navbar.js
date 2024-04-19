d9bfb720ae252425ffca9c8c5537f8590d0376db
Tetsuhiryo <156308083+Tetsuhiryo@users.noreply.github.com>
2024-04-20 00:14:42 +0800

    Delete navbar.js

--- navbar.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    var navbar = document.getElementById("navbar");
-    if (window.pageYOffset > 100) {
-       navbar.classList.add("hidden");
-    } else {
-       navbar.classList.remove("hidden");
-    }
-   };
